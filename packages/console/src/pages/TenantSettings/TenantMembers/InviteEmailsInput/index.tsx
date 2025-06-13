84b51018f5752895c1e2c436db80c54f3a73d2d1
hekpac <h.paches@gmail.com>
2025-06-13 22:03:44 +0200

    refactor(console): use MultiOptionInput for invite emails

--- packages/console/src/pages/TenantSettings/TenantMembers/InviteEmailsInput/index.tsx
+++ packages/console/src/pages/TenantSettings/TenantMembers/InviteEmailsInput/index.tsx
@@ -1,44 +1,27 @@
 import { emailRegEx } from '@logto/core-kit';
 import { generateStandardShortId } from '@logto/shared/universal';
-import { conditional, type Nullable } from '@silverhand/essentials';
-import classNames from 'classnames';
-import { useEffect, useRef, useState } from 'react';
+import { conditional } from '@silverhand/essentials';
 import { useFormContext } from 'react-hook-form';
 
-import Close from '@/assets/icons/close.svg?react';
-import IconButton from '@/ds-components/IconButton';
-import Tag from '@/ds-components/Tag';
-import { onKeyDownHandler } from '@/utils/a11y';
+import MultiOptionInput from '@/components/MultiOptionInput';
 
 import type { InviteeEmailItem } from '../types';
 
 import styles from './index.module.scss';
 
-type Props = {
+export type Props = {
   readonly formName?: string;
   readonly className?: string;
   readonly values: InviteeEmailItem[];
   readonly onChange: (values: InviteeEmailItem[]) => void;
   readonly error?: string | boolean;
   readonly placeholder?: string;
-  /**
-   * Function to check for duplicated or invalid email addresses. It should return valid email addresses
-   * and an error message if any.
-   */
   readonly parseEmailOptions: (values: InviteeEmailItem[]) => {
     values: InviteeEmailItem[];
     errorMessage?: string;
   };
 };
 
-/**
- * The body-2 font declared in @logto/core-kit/scss/fonts. It is referenced here to calculate
- * the width of the input text, which determines the minimum width of the input field.
- */
-const fontBody2 =
-  '400 14px / 20px -apple-system, system-ui, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif, Apple Color Emoji';
-
-// TODO: @Charles refactor me, use `<MultiOptionInput />` instead.
 function InviteEmailsInput({
   formName = 'emails',
   className,
@@ -48,142 +31,47 @@ function InviteEmailsInput({
   placeholder,
   parseEmailOptions,
 }: Props) {
-  const ref = useRef<HTMLInputElement>(null);
-  const [focusedValueId, setFocusedValueId] = useState<Nullable<string>>(null);
-  const [currentValue, setCurrentValue] = useState('');
   const { setError, clearErrors } = useFormContext();
-  const [minInputWidth, setMinInputWidth] = useState<number>(0);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  useEffect(() => {
-    // Render placeholder text in canvas to calculate its width in CSS pixels.
-    const ctx = canvasRef.current?.getContext('2d');
-    if (!ctx) {
-      return;
-    }
-    ctx.font = fontBody2;
-    setMinInputWidth(ctx.measureText(currentValue).width);
-  }, [currentValue]);
-
-  const onChange = (values: InviteeEmailItem[]): boolean => {
-    const { values: parsedValues, errorMessage } = parseEmailOptions(values);
-
+  const handleChange = (nextValues: InviteeEmailItem[]) => {
+    const { values: parsedValues, errorMessage } = parseEmailOptions(nextValues);
     if (errorMessage) {
       setError(formName, { type: 'custom', message: errorMessage });
-      return false;
+    } else {
+      clearErrors(formName);
     }
-
-    clearErrors(formName);
     rawOnChange(parsedValues);
-    return true;
-  };
-
-  const handleAdd = (value: string) => {
-    const newValues: InviteeEmailItem[] = [
-      ...values,
-      {
-        value,
-        id: generateStandardShortId(),
-        ...conditional(!emailRegEx.test(value) && { status: 'error' }),
-      },
-    ];
-    if (onChange(newValues)) {
-      setCurrentValue('');
-      ref.current?.focus();
-    }
-  };
-
-  const handleDelete = (option: InviteeEmailItem) => {
-    onChange(values.filter(({ id }) => id !== option.id));
   };
 
   return (
-    <>
-      <div
-        className={classNames(styles.input, Boolean(error) && styles.error, className)}
-        role="button"
-        tabIndex={0}
-        onKeyDown={onKeyDownHandler(() => {
-          ref.current?.focus();
-        })}
-        onClick={() => {
-          ref.current?.focus();
-        }}
-      >
-        <div className={styles.wrapper}>
-          {values.map((option) => (
-            <Tag
-              key={option.id}
-              variant="cell"
-              className={classNames(
-                styles.tag,
-                option.status && styles[option.status],
-                option.id === focusedValueId && styles.focused
-              )}
-              onClick={() => {
-                ref.current?.focus();
-              }}
-            >
-              {option.value}
-              <IconButton
-                className={styles.delete}
-                size="small"
-                onClick={() => {
-                  handleDelete(option);
-                }}
-                onKeyDown={onKeyDownHandler(() => {
-                  handleDelete(option);
-                })}
-              >
-                <Close className={styles.close} />
-              </IconButton>
-            </Tag>
-          ))}
-          <input
-            ref={ref}
-            placeholder={conditional(values.length === 0 && placeholder)}
-            value={currentValue}
-            style={{ minWidth: `${minInputWidth + 10}px` }}
-            onKeyDown={(event) => {
-              if (event.key === 'Backspace' && currentValue === '') {
-                if (focusedValueId) {
-                  onChange(values.filter(({ id }) => id !== focusedValueId));
-                  setFocusedValueId(null);
-                } else {
-                  setFocusedValueId(values.at(-1)?.id ?? null);
-                }
-                ref.current?.focus();
-              }
-              if (event.key === ' ' || event.code === 'Space' || event.key === 'Enter') {
-                // Focusing on input
-                if (currentValue !== '' && document.activeElement === ref.current) {
-                  handleAdd(currentValue);
-                }
-                // Do not react to "Enter"
-                event.preventDefault();
-              }
-            }}
-            onChange={({ currentTarget: { value } }) => {
-              setCurrentValue(value);
-              setFocusedValueId(null);
-            }}
-            onFocus={() => {
-              ref.current?.focus();
-            }}
-            onBlur={() => {
-              if (currentValue !== '') {
-                handleAdd(currentValue);
-              }
-              setFocusedValueId(null);
-            }}
-          />
-        </div>
-      </div>
-      {Boolean(error) && typeof error === 'string' && (
-        <div className={styles.errorMessage}>{error}</div>
-      )}
-      <canvas ref={canvasRef} />
-    </>
+    <MultiOptionInput<InviteeEmailItem>
+      className={className}
+      values={values}
+      valueClassName={(option) => option.status ? styles[option.status] : undefined}
+      getId={(option) => option.id}
+      renderValue={(option) => option.value}
+      validateInput={(text) => {
+        const item: InviteeEmailItem = {
+          id: generateStandardShortId(),
+          value: text,
+          ...conditional(!emailRegEx.test(text) && { status: 'error' }),
+        };
+        const { errorMessage } = parseEmailOptions([...values, item]);
+        if (errorMessage) {
+          return errorMessage;
+        }
+        return { value: item };
+      }}
+      placeholder={placeholder}
+      error={error}
+      onChange={handleChange}
+      onError={(err) => {
+        setError(formName, { type: 'custom', message: err });
+      }}
+      onClearError={() => {
+        clearErrors(formName);
+      }}
+    />
   );
 }
 
